428c1a6ff4d576b6f968f33061a6242d3280e8c5
Pub <96320073+b3Rhunter@users.noreply.github.com>
2023-10-04 18:12:37 -0500

    Add files via upload

--- commands/fun/ethers.js
+++ commands/fun/ethers.js
@@ -8,21 +8,16 @@ module.exports = {
         .setDescription('Get the current block number and gas price from the Ethereum network'),
     async execute(interaction) {
         try {
-            // Connect to the default provider (mainnet)
             const provider = new ethers.providers.InfuraProvider('mainnet');
             console.log(provider)
-            // Get the current block number and gas price
             const [blockNumber, gasPrice] = await Promise.all([
                 provider.getBlockNumber(),
                 provider.getGasPrice()
             ]);
 
-            // Convert gas price from wei to gwei for better readability
             const gasPriceGwei = ethers.utils.formatUnits(gasPrice, 'gwei');
-            // Convert gas price to an integer to remove decimal places
             const gasPriceGweiInteger = parseInt(gasPriceGwei);
             console.log(gasPriceGweiInteger)
-            // Reply with the information
             await interaction.reply(`Block Number: ${blockNumber}\nGas: ${gasPriceGweiInteger} gwei`);
         } catch (error) {
             console.error(error);
